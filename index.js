e2398e4476a97ddc2990270662bc47fc8bccea6c
Stack <68664917+StackTheDev@users.noreply.github.com>
2020-12-06 20:12:31 +0100

    Update index.js

--- index.js
+++ index.js
@@ -93,6 +93,170 @@ bot.on("message", async message => {
         }
 
 
+<<<<<<< Updated upstream
+=======
+
+
+
+
+
+
+        /** Logs */
+        if(cmd === `${prefix}say`){
+            let msg = args.slice(0)
+            
+            message.channel.send(msg)
+
+            const logchannel = message.guild.channels.cache.find(channel => channel.id === "LOG_CHANNEL_ID")
+
+            /** logchannel.send(`${message.author} Just did the say command! The message was **<${msg}>**`) */
+
+            const logembed = new Discord.MessageEmbed()
+            .setTitle("Say Command Done")
+            .setColor("RED")
+            .setFooter(`${message.guild.name}`)
+
+            .addFields(
+                { name: `Command`, value: `Say Command`},
+                { name: `Author`, value: `${message.author}`},
+                { name: `Message`, value: `${msg}`},
+            )
+            logchannel.send(logembed)
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        /** Warning Command */
+
+        if(cmd === `${prefix}warn`){
+            if(message.member.roles.cache.has("PERMISSION_ROLE")){
+            let user = message.mentions.members.first();
+            let warning = args.slice(23)
+
+            user.send(`You have been warned in __${message.guild.name}__ for the reason: **${warning}**`)
+
+            const logchannel = message.guild.channels.cache.find(channel => channel.id === "LOG_CHANNEL_ID")
+            /** logchannel.send(`The user ${user} just got warned by ${message.author} for the reason **<${warning}>**`) */
+
+            const logembed = new Discord.MessageEmbed()
+            .setTitle("User Warned")
+            .setColor("RED")
+            .setFooter(`- ${message.guild.name}`)
+            .addFields(
+                { name: `Warned By`, value: `${message.author}`},
+                { name: `Warning To`, value: `${user}`},
+                { name: `Warning`, value: `${warning}`},
+            )
+            logchannel.send(logembed)
+
+        } else {
+            const logchannel = message.guild.channels.cache.find(channel => channel.id === "LOG_CHANNEL_ID")
+            message.reply("You dont have the permissions for this command!")
+            logchannel.send(`${message.author} Just tried to do the warn command!`)
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        /** Verify System */
+        if(message.channel.id === "VERIFY_CHANNEL_ID"){
+        if(cmd === `${prefix}verify`){
+            message.delete( {timeout: 5000} )
+            const verifyrole = message.guild.roles.cache.find(role => role.id === "VERIFY_ROLE_ID")
+
+            message.member.roles.add(verifyrole)
+            message.reply("You have been verified and the role have been added to you user!").then(msg => msg.delete( {timeout: 10000} ))
+
+            const logchannel = message.guild.channels.cache.find(channel => channel.id === "LOG_CHANNEL_ID")
+            logchannel.send(`The user ${message.author} just verified!`)
+            
+        }
+    }
+
+    /** AdminVerify */
+    if(cmd === `${prefix}adminverify`){
+        message.delete( {timeout: 5000} )
+        if(message.member.roles.cache.has("PERMISSION_ROLE")){
+        let user = message.mentions.members.first()
+        const verifyrole = message.guild.roles.cache.find(role => role.id === "VERIFY_ROLE_ID")
+
+        user.roles.add(verifyrole)
+        message.channel.send(`${user} Was verified by ${message.author}`).then(msg => msg.delete( {timeout: 10000} ))
+
+        const logchannel = message.guild.channels.cache.find(channel => channel.id === "LOG_CHANNEL_ID")
+        logchannel.send(`${user} Just got Admin-verified by ${message.author}`)
+
+    } else {
+        message.reply("You dont have the permissions for that command!").then(msg => msg.delete( {timeout: 5000} ))
+    }
+}
+
+
+
+    /** Say Command */
+    if(cmd === `${prefix}msgsay`){
+        let msg = args.slice(0)
+
+        message.channel.send(msg)
+    }
+
+    /** Say Embed Command */
+    if(cmd === `${prefix}embedsay`){
+        let msg = args.slice(0)
+
+        const embed = new Discord.MessageEmbed()
+        .setAuthor(`By ${message.author.tag}`, message.author.avatarURL())
+        .setDescription(msg)
+    
+        message.channel.send(embed)
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+>>>>>>> Stashed changes
 })
 
 bot.login(botsettings.token);
